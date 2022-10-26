dc3ea1b52f9ff10fdde867cdea52ebc7ef6b77c9
techietohid <81966351+techietohid@users.noreply.github.com>
2022-10-26 15:57:58 +0330

    Update

--- Assets/Script.js
+++
@@ -1,4 +0,0 @@
-[{"id":"a-17-p","type":"SCROLL_PROGRESS","parameterLabel":
-"Scroll","continuousActionGroups":[
-    {"keyframe":0,"actionItems":[{"id":"a-17-n",
-"actionTypeId":"TRANSFORM_MOVE","config":{"delay":0,"easing":"","duration":500,"target":{"useEventTarget":"CHILDREN","selector":".glass-container.is-work","selectorGuids":[""]},"yValue":100,"xUnit":"PX","yUnit":"px","zUnit":"PX"}}]},{"keyframe":100,"actionItems":[{"id":"a-17-n-2","actionTypeId":"TRANSFORM_MOVE","config":{"delay":0,"easing":"","duration":500,"target":{"useEventTarget":"CHILDREN","selector":".glass-container.is-work"},"yValue":-100,"xUnit":"PX","yUnit":"px","zUnit":"PX"}}]}]}]
